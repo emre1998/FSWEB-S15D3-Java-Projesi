84effbd217373b747df15d86b4d74b0bd3551c69
Emre Can Kızılateş <kizilatesemrecan@gmail.com>
2023-08-12 22:30:31 +0300

    final updates in java maps vol2

--- src/com/workintech/main/Main.java
+++ src/com/workintech/main/Main.java
@@ -1,5 +1,51 @@
+package com.workintech.main;
+
+import com.workintech.models.Employee.Employee;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        LinkedList<Employee> employees = new LinkedList<>();
+
+        Employee emp1 = new Employee(1,"Ahmet","Yılmaz");
+        Employee emp2 = new Employee(2,"Mehmet" ,"Kara");
+        Employee emp3 = new Employee(1,"Ahmet","Yılmaz");
+        Employee emp4 = new Employee(1,"İzel","Ercan");
+        Employee emp5 = new Employee(5,"İbrahim","Tatlıses");
+
+        employees.add(emp1);
+        employees.add(emp2);
+        employees.add(emp3);
+        employees.add(emp4);
+        employees.add(emp5);
+
+        LinkedList<Employee> duplicateEmployees = new LinkedList<>();
+        HashMap<Integer,Employee> uniqueEmployeeMap = new HashMap<>();
+
+        Iterator<Employee> iterator = employees.iterator();
+        while(iterator.hasNext()){
+            Employee employee = iterator.next();
+            if(uniqueEmployeeMap.containsKey(employee.getId())) {
+                duplicateEmployees.add(employee);
+                iterator.remove();
+            }else {
+                uniqueEmployeeMap.put(employee.getId(),employee);
+            }
+        }
+
+        System.out.println("Tekrar eden Employee'ler: ");
+        for (Employee duplicateEmployee : duplicateEmployees) {
+            System.out.println(duplicateEmployee.getFullName());
+        }
+
+        System.out.println("\nBenzersiz Employee'ler: ");
+        for (Employee uniqueEmployee : uniqueEmployeeMap.values()) {
+            System.out.println(uniqueEmployee.getFullName());
+        }
+
     }
 }
--- src/com/workintech/models/Employee/Employee.java
+++ src/com/workintech/models/Employee/Employee.java
@@ -1,2 +1,37 @@
-package com.workintech.models.Employee;public class Employee {
+package com.workintech.models.Employee;
+
+import java.util.Objects;
+
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Employee(int id,String firstName,String lastName) {
+        this.id=id;
+        this.firstName= firstName;
+        this.lastName = lastName;
+    }
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id && Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName);
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
 }
+
+
